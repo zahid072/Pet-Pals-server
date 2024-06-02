164c0e72ed0855e98a0c01b43f4e845ee440c7be
zahid072 <jahidhasan19368@gmail.com>
2024-06-02 19:59:13 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -12,6 +12,42 @@ app.get("/", (req, res) => {
   res.send("pet adoption site server is running.......");
 });
 
+// mongodb
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvtbyiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    // all apis
+    
+   
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.listen(port, (req, res) => {
   console.log(`server is running on port: ${port}`);
 });
