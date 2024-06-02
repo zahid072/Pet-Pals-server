af93913565f94752caa317821b1bb26f88fc81a2
zahid072 <jahidhasan19368@gmail.com>
2024-06-03 01:56:40 +0600

    create users post apis

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.get("/", (req, res) => {
 
 // mongodb
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvtbyiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,7 +23,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -32,21 +31,31 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("PetPalsDb").collection("allUsers");
 
     // all apis
-    
-   
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const filter = {email: user.email}
+      const existUser = await usersCollection.findOne(filter)
+      if(existUser){
+        return res.send({message: "User already exist."})
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.listen(port, (req, res) => {
   console.log(`server is running on port: ${port}`);
