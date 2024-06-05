1b616ef8d8f08aabf9f5b6a458794faa260a132e
zahid072 <jahidhasan19368@gmail.com>
2024-06-05 19:16:49 +0600

    add infinite scrolling method

--- index.js
+++ index.js
@@ -50,11 +50,43 @@ async function run() {
     });
 
     app.get("/pets", async (req, res) => {
-      const result = await petsCollection
-        .find()
-        .sort({ timestamp: -1 })
-        .toArray();
-      res.send(result);
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = (page - 1) * limit;
+    
+      try {
+        const result = await petsCollection
+          .find()
+          .sort({ timestamp: -1 })
+          .skip(skip)
+          .limit(limit)
+          .toArray();
+          
+        const totalCount = await petsCollection.countDocuments();
+    
+        res.send({
+          pets: result,
+          petsCount: totalCount,
+        });
+      } catch (error) {
+        res.status(500).send({ error: 'Failed to fetch pets' });
+      }
+    });
+    app.get("/pets/search", async (req, res) => {
+      const { name } = req.query;
+      try {
+        let result;
+        if (name) {
+          result = await petsCollection.find({ petName: new RegExp(name, "i") })
+            .sort({ timestamp: -1 })
+            .toArray();
+        } else {
+          result = await petsCollection.find().sort({ timestamp: -1 }).toArray();
+        }
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: "Error fetching pets", error });
+      }
     });
     // post apis
     app.post("/users", async (req, res) => {
